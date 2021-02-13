8df688ff7a1d18366fb364ab4a8ecd60732b1891
unknown <warhoundmp@gmail.com>
2021-02-13 22:25:21 +0800

    test 154

--- commands/tuto.js
+++ commands/tuto.js
@@ -6,6 +6,7 @@ function tuto(client, message, args, pics)
 	const black = client.emojis.cache.find(emoji => emoji.name === "black");
 	const rose = client.emojis.cache.find(emoji => emoji.name === "rose");
 	const jade = client.emojis.cache.find(emoji => emoji.name === "jade");
+	const red = client.emojis.cache.find(emoji => emoji.name === "red");
 
 	let lista = [' '];
 	lista.push(`${pics[1]}`);
@@ -25,11 +26,12 @@ function tuto(client, message, args, pics)
 
 	
 
-	actualtext.push('Brushstroke Dice are color-coded d10s, each color representing a result:');
-	actualtext.push(`${black}`+'Black Jade counts as 1 success with 1 involvement.');
+	actualtext.push('Brushstroke Dice are color-coded d10s, each color representing a result
+	actualtext.push(`${red}`+'Blood Jade adds 1 involvement. **if 10 (max) involvement, counts as 2 hits.**');
+	actualtext.push(`${black}`+'Black Jade counts as 1 hit with 1 involvement.');
 	actualtext.push(`${rose}`+'Rose Jade counts as a blossom.');
-	actualtext.push(`${white}`+'White Jade counts as 1 success.');
-	actualtext.push(`${jade}`+'Pure Jade removes 1 involvement. **if 0 involvement, counts as 2 successes.**');
+	actualtext.push(`${white}`+'White Jade counts as 1 hit.');
+	actualtext.push(`${jade}`+'Pure Jade removes 1 involvement. **if no involvement, counts as 2 hits.**');
 
 
 	message.channel.send(actualtext);
